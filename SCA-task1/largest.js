9df0133b64a92819d00ea78f8ec848e9bf78e1c3
AdelabuAderonke <adelabu.aderonke9638@gmail.com>
2020-06-12 07:38:30 +0200

    Assignment one

---
+++ SCA-task1/largest.js
@@ -0,0 +1,13 @@
+const largest = (a,b)=>{
+ if(a > b){
+     console.log(a+ " is larger than"+b);
+ }
+ else if(b > a){
+    console.log(b+ " is larger than"+a);
+
+ }
+ else{
+     console.log("They are equal value");
+ }
+}
+largest(10,10);
